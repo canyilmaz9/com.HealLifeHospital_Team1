7572974dbef82e4fd441d7db2960eb26baa4872f
Ismail KILGOZ <ligerhyme@hotmail.com>
2024-11-22 17:52:49 +0300

    Ismail/22.11/V1.0

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -25,5 +26,38 @@ public class LoginPage extends BasePage {
 		type(passwordInput, password);
 		click(loginButton);
 	}
+
+	@FindBy(xpath = "//*[text()='Login']")
+	public WebElement homePageLoginButton;
+
+	@FindBy(xpath = "//*[@class=\"loginbg\"]")
+	private WebElement loginBox;
+
+	@FindBy(xpath = "//*[@class=\"col-lg-6 col-sm-6 col-sm-6\"]")
+	private WebElement latestNewBox;
+
+	public void loginPageBoxes(){
+		Assert.assertTrue(loginBox.isDisplayed());
+		Assert.assertTrue(latestNewBox.isDisplayed());
+
+	}
+
+	@FindBy(id = "email")
+	private WebElement usernameBox;
+
+	@FindBy(id = "password")
+	private WebElement passwordBox;
+
+	@FindBy(className = "btn")
+	private WebElement signInButton;
+
+	public void setLogin(String username, String password) {
+		Assert.assertTrue(usernameBox.isDisplayed());
+		Assert.assertTrue(passwordBox.isDisplayed());
+		type(usernameBox, username);
+		type(passwordBox, password);
+		click(signInButton);
+	}
+
 }
 
--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -1,12 +1,19 @@
 package stepdefinitions;
 
+import config.ConfigReader;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
+import utilities.ReusableMethods;
 
 public class LoginSteps {
+	private static final Logger logger = LogManager.getLogger(GoogleSteps.class);
 	WebDriver driver = stepdefinitions.Hooks.getDriver();
 	LoginPage loginPage = new LoginPage(driver);
 
@@ -23,5 +30,46 @@ public class LoginSteps {
 	@Then("the user should see the home page")
 	public void theUserShouldSeeTheHomePage() {
 		// Assertion logic
+
+
+	}
+
+	@Given("kullanici anasayfada")
+	public void kullaniciAnasayfada() {
+		driver.get(ConfigReader.getProperty("homePageUrl"));
+		String homePageUrl = ConfigReader.getProperty("homePageUrl");
+		logger.info("Heal Life Hospital ana sayfasına gidildi: " + homePageUrl );
+		Assert.assertEquals(ConfigReader.getProperty("homePageUrl"),driver.getCurrentUrl());
+		logger.info("Heal Life Hospital ana sayfasında oldugu dogrulandi" );
+	}
+
+	@When("login butonu gorunur ve tiklanabilir oldugu dogrulanir ve tiklanir. tiklandiktan sonra userlogin sayfasinda oldugu dogrulanir")
+	public void loginButonuGorunurVeTiklanabilirOlduguDogrulanirVeTiklanirTiklandiktanSonraUserloginSayfasindaOlduguDogrulanir() {
+		Assert.assertTrue(loginPage.homePageLoginButton.isDisplayed());
+		logger.info("Ana sayfada login butonu goruntulendi" );
+		Assert.assertTrue(loginPage.homePageLoginButton.isEnabled());
+		logger.info("Ana sayfada login butonu tiklanabilirligi dogrulandi" );
+		loginPage.homePageLoginButton.click();
+		logger.info("Ana sayfada login butonu tiklandi" );
+		String loginPageUrl = ConfigReader.getProperty("loginPageUrl");
+		Assert.assertEquals(ConfigReader.getProperty("loginPageUrl"),loginPageUrl);
+		logger.info("Login page sayfasına gidildigi dogrulandi: " + loginPageUrl );
+	}
+
+	@And("user login sayfasinda sagda login penceresi solda latest new bilgileri bulunur")
+	public void userLoginSayfasindaSagdaLoginPenceresiSoldaLatestNewBilgileriBulunur() {
+		loginPage.loginPageBoxes();
+		logger.info("user login sayfasinda sagda login penceresi solda latest new bilgileri bulundu" );
+	}
+
+	@Then("user login sayfasinda \\(sisteme daha önceden kayitli) bilgiler girilerek dashboard panele login olunur.")
+	public void userLoginSayfasindaSistemeDahaÖncedenKayitliBilgilerGirilerekDashboardPaneleLoginOlunur() {
+		loginPage.setLogin(ConfigReader.getProperty("patientUsername"),ConfigReader.getProperty("patientPassword"));
+
+		String patientDashboardUrl=ConfigReader.getProperty("patientDashboardUrl");
+		Assert.assertEquals(patientDashboardUrl,driver.getCurrentUrl());
+		ReusableMethods.bekle(2);
+
+
 	}
 }
