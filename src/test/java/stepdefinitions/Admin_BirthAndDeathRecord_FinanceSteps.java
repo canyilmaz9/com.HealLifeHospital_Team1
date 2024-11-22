edd2fa8d56ef4089998429797e04376e984d98d1
Can Yilmaz <canyilmaz9999@gmail.com>
2024-11-22 11:59:32 +0100

    merge islemi

--- src/test/java/stepdefinitions/Admin_BirthAndDeathRecord_FinanceSteps.java
+++ src/test/java/stepdefinitions/Admin_BirthAndDeathRecord_FinanceSteps.java
@@ -3,8 +3,6 @@ package stepdefinitions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import pages.AboutUsPage;
-import pages.Admin_BirthAndDeathRecord_FinancePage;
 
 public class Admin_BirthAndDeathRecord_FinanceSteps {
     private static final Logger logger = LogManager.getLogger(AboutUsSteps.class);
